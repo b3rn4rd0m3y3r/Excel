a8d155cf7a9cc443b7eeefa56a5b895992ff9bce
Bernardo Meyer <55299005+b3rn4rd0m3y3r@users.noreply.github.com>
2022-12-26 21:22:00 -0300

    Add files via upload

--- LePlan32/LeBanqueiro1.php
+++ LePlan32/LeBanqueiro1.php
@@ -5,16 +5,13 @@
   echo "<br>";
   // Sql
   $sql = "SELECT [Id],[UF],[Cidade],[CPF],[Conta],[Valor] FROM [tab1$] ";
-  $sql = $sql . " ORDER BY [UF], [Cidade], [CPF], [Conta]";
   $stmt = odbc_exec($conn, $sql);
   var_dump($stmt);
-  echo "<table border=1 cellspacing=0 cellpadding=4>";
-  echo "<tr><th>Id<th>UF<th>Cidade<th>CPF<th>Conta<th>Valor</tr>";
+  echo "<br>";
   // Loop row register
   // Pode tentar odbc_fetch_row também
-  $t = "</td><td>";
   while($row = odbc_fetch_array($stmt)){
-      echo "<tr><td>" . $row['Id'] . $t . $row['UF'] . $t . $row['Cidade'] . $t . $row['CPF']. $t . $row['Conta'] . $t . $row['Valor'] . "</td></tr>";
+      echo $row['Id'] . " - " . $row['UF'] . " - " . $row['Cidade'] . " - " . $row['CPF']. " - " . $row['Conta'] . " - " . $row['Valor'] . "<br>";
       }
-  echo "</table>";
+
 ?>
