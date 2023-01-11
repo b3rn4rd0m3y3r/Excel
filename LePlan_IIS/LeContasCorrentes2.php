f401f2f65e59b9226751d2db3f773c977bb631f4
Bernardo Meyer <55299005+b3rn4rd0m3y3r@users.noreply.github.com>
2023-01-11 11:30:14 -0300

    Add files via upload

---
+++ LePlan_IIS/LeContasCorrentes2.php
@@ -0,0 +1,64 @@
+<?php
+  // Header
+  header('Content-Type: text/html; charset=iso-8859-1');
+  // Pega o nome da tabela na URL
+  if( $_GET["Planilha"] ) {
+	$PLAN_get = $_GET["Planilha"];
+	} else {
+	$PLAN_get = "";
+	}
+  if( $PLAN_get == "" ) {
+	exit("Nome da planilha a ser listada ausente.");
+	}
+  if( $_GET["Ordem"] ) {
+	$ORD_get = $_GET["Ordem"]; 
+	} else {
+	$ORD_get = "";
+	}
+  // Conexão
+  $conn = odbc_connect("Driver={Microsoft Excel Driver (*.xls)};DriverId=790;Dbq=" . $PLAN_get . ";DefaultDir=C:\\Inetpub\\wwwroot\\dbase" , '', '');
+  //var_dump($conn);
+  echo "<br>";
+  // Sql
+  $sql = "SELECT TOP 1 * FROM [tab1$] ";
+  $stmt = odbc_exec($conn, $sql);
+  $row = odbc_fetch_array($stmt);
+  // Array dos campos
+  $arrCmps = array(); // Campos lidos
+  $arrCols = array(); // NOVO - Títulos das colunas para exibição
+  // Loop to get fields
+  $ind = 0;
+  foreach( $row as $key=>$valor ){
+	$chave = explode("_", $key); // NOVO
+	$arrCols[$ind] = $chave[0]; // NOVO
+	$arrCmps[$ind] = $key; 
+	$ind++;
+	}
+  print_r($arrCmps);
+  // Re-seleciona todos os registros
+  $sql = "SELECT * FROM [tab1$] ";
+  if( $ORD_get != "" ){
+	$sql = $sql . " ORDER BY " . $ORD_get;
+	}
+  $stmt = odbc_exec($conn, $sql);
+  echo "<table border=1 cellpadding=3 cellspacing=0>";
+  echo "<tr>";
+  foreach( $arrCols as $key=>$valor ){ // Alterado array para $arrCols
+	echo "<th>" . $valor . "</th>";
+	}  
+  echo "</tr>";
+  
+  // Loop row register
+  // Pode tentar odbc_fetch_row também
+  while($row = odbc_fetch_array($stmt)){
+	echo "<tr>";
+	//echo $row['Id'] . " - " . $row['UF'] . " - " . $row['Cidade'] . " - " . $row['CPF']. " - " . $row['Conta'] . " - " . $row['Valor'] . "<br>";
+	foreach( $arrCmps as $key=>$valor ){
+		echo "<td>" . $row[$valor] . "</td>";
+		}  	
+	echo "</tr>";
+	}
+	
+  echo "</table>";
+
+?>
