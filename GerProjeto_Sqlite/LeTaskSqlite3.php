9252782fafa315082a923c29ba304905168016f6
Bernardo Meyer <55299005+b3rn4rd0m3y3r@users.noreply.github.com>
2023-02-21 19:46:13 -0300

    Add files via upload

--- GerProjeto_Sqlite/LeTaskSqlite3.php
+++ GerProjeto_Sqlite/LeTaskSqlite3.php
@@ -14,13 +14,11 @@
   /*
 	CHAMADA:
 	
-	http://localhost/dbapp/LeContasCorrentes8.php?
-	Planilha=ContasCorrentes.xls
-	&Ordem=UF_t,Cidade_t,CPF_t
-	&Quebra=UF_t,Cidade_t
-	&Somas=Valor_f_2,Desconto_f_2
+	http://localhost/dbapp/LeTaskSqlite3.php?
+	Planilha=Projetos.db
 	
   */
+  // 1 - DEFINIÇÕES INICIAIS
   // Header
   header('Content-Type: text/html; charset=iso-8859-1');
   // Pega o nome da tabela na URL
@@ -29,7 +27,7 @@
 	} else {
 	$PLAN_get = "";
 	}
-   // Conexão
+   // 2 - CONEXÃO AO BANCO DE DADOS
   try {
 	//$odbt =  "sqlite:./" . $PLAN_get;
 	$odbt = 'sqlite:'. __DIR__ .'\Projetos.db';
@@ -40,11 +38,11 @@
 	}
   print_r($conn);
   echo "<br>";
-  // Sql
+  // 3 - CONSTRUÇÃO/EXECUÇÃO DO SQL
   $sql = "SELECT * FROM Tasks ORDER BY Id";
   $stmt = $conn->prepare($sql);
   $res = $stmt->execute();
-  // Máximos e mínimos
+  // 4 - VALOR INICIAL DA MENOR E DA MAIOR DATA
   $MAIOR_data = strtotime('2000-12-31 00:00:00');
   $MENOR_data = strtotime('2037-12-31 00:00:00');
   echo "MIN<br>";
@@ -53,7 +51,8 @@
   echo "MAX<br>";
   print_r($MAIOR_data);
   echo "<br>";
-  // Primeiro loop
+  // 5 - LEITURA DE TODOS OS REGISTROS E
+  //       DESCOBERTA DA MENOR E DA MAIOR DATA
   while($row = $stmt->fetch()){
 	$DtIni = $row["DtInicial"];
 	$DtFim = $row["DtFinal"];
@@ -77,20 +76,22 @@
   $res = $stmt1->execute();
   echo "<br>No Linhas: " . $NoLin;
   $lin = 0;
+  // 6 - LEITURA DE TODOS OS REGISTROS E
+  //       POSICIONAMENTO DAS BARRAS
   echo "<div id=main>";
   while($row = $stmt1->fetch()){
   	$DtIni = $row["DtInicial"];
 	$DtFim = $row["DtFinal"];
 	$Xini = (strtotime($DtIni) - $MENOR_data)/$UmDia*40;
-	//echo $DtIni . ">" . $DtFim . ":" . $row["Tarefa"] . " | " . $row["Dias"] . "<br>";
-	//echo $Xini . ">" . $row["Dias"] . ":" . $row["Tarefa"] . " | " . $row["Dias"] . "<br>";
 	$Dias = (strtotime($DtFim)-strtotime($DtIni))/$UmDia+1;
 	echo "<div class=bar style=\"top:" . $lin*35 . "px;height:30px;left:" . $Xini . "px;width:" . $Dias*40 . "px;\">" . $row["Tarefa"] . "</div>";
 	$lin++;
 	}
+  // 7 - COLOCAÇÃO DAS BARRAS VERTICAIS
+  //       DO GRÁFICO DE GANTT
   $NoLin = $lin;
   for($lin=0;$lin<(int)($MAIOR_data-$MENOR_data)/$UmDia+1;$lin++){	
-	echo "<div style=\"left: " . $lin*40 .  "px;height: " . $NoLin*35 .  "px\" class=traco>&nbsp;</div>";
+	echo "<div class=traco style=\"left: " . $lin*40 .  "px;height: " . $NoLin*35 .  "px\">&nbsp;</div>";
 	}
   echo "</div>";
 ?>
